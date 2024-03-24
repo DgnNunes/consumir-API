452b21b88fd57b52ba66c679fa8d359d7729986a
Douglas Gilson Nunes <douglas_g_nunes@estudante.sesisenai.org.br>
2024-03-24 16:54:28 -0300

    #

---
+++ index.js
@@ -0,0 +1,9 @@
+let muscle = 'biceps';
+
+fetch('https://api.api-ninjas.com/v1/exercises?muscle=' + muscle, {
+    method: 'GET',
+    headers: { 'X-Api-Key': 'r39tgCKGIcWEgPClOnD1JeiydHJa5Kug2hP41Khp'}
+})
+.then((res) => res.json())
+.then((data) => console.log(data))
+.catch((error) => console.error('Error:', error));
